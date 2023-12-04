7b0431f636982c00b2425cb9613c3bf524e40b03
Travis Snapp <traviswsnapp@outlook.com>
2023-12-03 22:32:44 -0500

    migration

--- api/server.js
+++ api/server.js
@@ -1,7 +1,11 @@
-const express = require("express")
+const express = require("express");
+const carsRouter = require("./cars/cars-router");
+const server = express();
 
-const server = express()
+server.use(express.json());
+
+server.use("/api/cars", carsRouter);
 
 // DO YOUR MAGIC
 
-module.exports = server
+module.exports = server;
